27a19072a69dc7f8cc259152bd03199b02c0fc23
Prajakta Rorigues <prajaktarodrigues@gmail.com>
2019-02-17 14:30:57 -0500

    Added tests/comments in IMconnectionTest

--- Development/ChatServer/src/test/java/tests/IMConnectionTest.java
+++ Development/ChatServer/src/test/java/tests/IMConnectionTest.java
@@ -1,53 +1,66 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import edu.northeastern.ccs.im.client.IMConnection;
 import edu.northeastern.ccs.im.client.IllegalNameException;
+import edu.northeastern.ccs.im.client.IllegalOperationException;
 import edu.northeastern.ccs.im.client.InvalidListenerException;
 import edu.northeastern.ccs.im.client.MessageScanner;
 import edu.northeastern.ccs.im.server.Prattle;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(IMConnection.class)
+/**
+ * The Class IMConnectionTest tests the methods provided IMConnection.
+ */
 public class IMConnectionTest {
 
-	public static ExecutorService executor;
+	/** The executor. */
+	private static ExecutorService executor;
 
+	/**
+	 * Setup server.
+	 */
 	@BeforeClass()
 	public static void setup() {
-        executor = Executors.newSingleThreadExecutor();
-        executor.execute(new Runnable() {
-            public void run() {
+		executor = Executors.newSingleThreadExecutor();
+		executor.execute(new Runnable() {
+			public void run() {
 				String[] args = {};
 				Prattle.main(args);
-				
-            }
-        });
+
+			}
+		});
 
 	}
 
+	/**
+	 * Kill server.
+	 *
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
 	@AfterClass()
 	public static void killSetup() throws InterruptedException {
-		executor.shutdownNow();
+		Prattle.stopServer();
+		if (null != executor) {
+			executor.shutdownNow();
+		}
 	}
 
+	/** The i M connection. */
 	private IMConnection iMConnection;
 
+	/**
+	 * Test create IM connection with null username.
+	 */
 	@Test
 	public void testCreateIMConnectionNullUsername() {
 
@@ -56,6 +69,9 @@ public class IMConnectionTest {
 		assert true;
 	}
 
+	/**
+	 * Test create IM connection with empty username.
+	 */
 	@Test
 	public void testCreateIMConnectionEmptyUsername() {
 
@@ -64,6 +80,9 @@ public class IMConnectionTest {
 		assert true;
 	}
 
+	/**
+	 * Test create IM connection with non empty username.
+	 */
 	@Test
 	public void testCreateIMConnectionNonEmptyUsername() {
 
@@ -71,6 +90,9 @@ public class IMConnectionTest {
 		assert true;
 	}
 
+	/**
+	 * Test add message listener with valid listener.
+	 */
 	public void testAddMessageListenerValidListener() {
 
 		iMConnection = new IMConnection("localhost", 4122, "prajakta");
@@ -78,6 +100,9 @@ public class IMConnectionTest {
 		assert true;
 	}
 
+	/**
+	 * Test add message with null listener.
+	 */
 	@Test(expected = InvalidListenerException.class)
 	public void testAddMessageListenerNull() {
 
@@ -85,30 +110,113 @@ public class IMConnectionTest {
 		iMConnection.addMessageListener(null);
 	}
 
-	@Test(expected = IllegalNameException.class)
-	public void testConnectInvalidUsername() throws Exception {
-		IMConnection iMConnectionSpy = PowerMockito.spy(new IMConnection("localhost", 4123, "prajakta_12"));
-		PowerMockito.doReturn(true).when(iMConnectionSpy, "login");
-		assertEquals(true, iMConnectionSpy.connect());
-	}
-
+	/**
+	 * Checks if is connection active when connection is not established.
+	 */
 	@Test
-	public void isConnectionActiveNullSocket() {
+	public void isConnectionActiveNotConnection() {
 		iMConnection = new IMConnection("localhost", 4127, "pra");
 		assertEquals(false, iMConnection.connectionActive());
 	}
 
+	/**
+	 * Checks if is connection active when connection is established.
+	 */
 	@Test
-	public void isConnectionctive() {
+	public void isConnectionActiveWhenConnected() {
 		iMConnection = new IMConnection("localhost", 4545, "pra");
 		iMConnection.connect();
 		assertEquals(true, iMConnection.connectionActive());
 	}
 
+	/**
+	 * Test connecting with invalid username.
+	 *
+	 * @throws Exception the exception while trying to connect
+	 *             
+	 */
+	@Test(expected = IllegalNameException.class)
+	public void testConnectInvalidUsername() throws Exception {
+		iMConnection = new IMConnection("localhost", 4545, "prajakta_12");
+		iMConnection.connect();
+
+	}
+
+	/**
+	 * Test connect failing with wrong port number.
+	 */
+	@Test
+	public void testConnectFail() {
+		iMConnection = new IMConnection("localhost", 4111, "omar");
+		assertFalse(iMConnection.connect());
+	}
+
+	/**
+	 * Test connect success.
+	 */
 	@Test
 	public void testConnectSuccess() {
 		iMConnection = new IMConnection("localhost", 4545, "pra");
 		assertEquals(true, iMConnection.connect());
 	}
 
+	/**
+	 * Test disconnect success.
+	 */
+	@Test
+	public void testDisconnectSuccess() {
+		iMConnection = new IMConnection("localhost", 4545, "pra");
+		iMConnection.connect();
+		iMConnection.disconnect();
+		assert true;
+	}
+
+	/**
+	 * Test send message.
+	 */
+	@Test
+	public void testSendMessage() {
+		iMConnection = new IMConnection("localhost", 4545, "sean");
+		iMConnection.connect();
+		iMConnection.sendMessage("hey I am testing");
+		assert true;
+	}
+
+	/**
+	 * Test sending message failed without making connection.
+	 */
+	@Test(expected = IllegalOperationException.class)
+	public void testSendMessageFailed() {
+		iMConnection = new IMConnection("localhost", 4545, "koka");
+		iMConnection.sendMessage("hey I am testing");
+	}
+	
+	/**
+	 * Test get keyboard scanner singleton instance.
+	 */
+	@Test
+	public void testGetKeyboardScanner() {
+		iMConnection = new IMConnection("localhost", 4545, "omar");
+		assertEquals(iMConnection.getKeyboardScanner(),iMConnection.getKeyboardScanner());
+	}
+	
+	
+	/**
+	 * Test message scanner exception before connecting.
+	 */
+	@Test(expected  = IllegalOperationException.class)
+	public void testMessageScannerException() {
+		iMConnection = new IMConnection("localhost", 4545, "omar");
+		assertEquals(null , iMConnection.getMessageScanner());
+	}
+
+	/**
+	 * Test message scanner after connecting.
+	 */
+	@Test
+	public void testMessageScanner() {
+		iMConnection = new IMConnection("localhost", 4545, "jane");
+		iMConnection.connect();
+		assertEquals(true , iMConnection.getMessageScanner() instanceof MessageScanner);
+	}
 }
