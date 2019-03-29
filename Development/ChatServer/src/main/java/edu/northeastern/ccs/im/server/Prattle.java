eca5492a732d21172b01158fd6cc7ca00a2125d7
koka <koka.v@husky.neu.edu>
2019-03-29 13:47:34 -0400

    change remove client on prattle

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/Prattle.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/Prattle.java
@@ -226,13 +226,11 @@ public abstract class Prattle {
   public static void removeClient(ClientRunnable dead) {
     // Test and see if the thread was in our list of active clients so that we
     // can remove it.
+
     if (authenticated.remove(dead.getUserId()) == null
-        || !active.remove(dead)
-        || !channelMembers.get(channelId).remove(dead)) {
+            || !active.remove(dead)
+            || !channelMembers.get(dead.getActiveChannelId()).remove(dead)) {
       ChatLogger.info("Could not find a thread that I tried to remove!\n");
-    }
-    if (channelMembers.get(channelId).isEmpty()) {
-      channelMembers.remove(channelId);
     }
     userRepository.setActive(false, dead.getUserId());
   }
